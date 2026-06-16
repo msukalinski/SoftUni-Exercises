74ad3273620b86d82ee771c38b2cfbee29adce79
msukalinski <msukalinski@gmail.com>
2026-06-16 20:20:41 +0300

    Delete JS Advanced/JS Applications Regular Exam - 30 March 2024/Cyberpunk Market directory

--- JS Advanced/JS Applications Regular Exam - 30 March 2024/Cyberpunk Market/src/middlewares/authMiddleware.js
+++
@@ -1,7 +0,0 @@
-import * as userService from '../services/userService.js';
-
-export const authMiddleware = (ctx, next) => {
-    ctx.user = userService.getUserData();
-
-    next();
-}
--- JS Advanced/JS Applications Regular Exam - 30 March 2024/Cyberpunk Market/src/services/userService.js
+++
@@ -1,3 +0,0 @@
-export const getUserData = () => JSON.parse(localStorage.getItem('userData'));
-export const setUserData = (data) => localStorage.setItem('userData', JSON.stringify(data));
-export const clearStorage = () => localStorage.clear();
